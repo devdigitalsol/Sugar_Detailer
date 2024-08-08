861b6895e75e5b930c59a331bc9ee94ce7550062
devdigitalsol <digital@solmc.in>
2024-08-08 14:50:32 +0530

    wip

--- src/components/Fifth.jsx
+++ src/components/Fifth.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import MyScratchCard from "./Scratch";
 import { useNavigate } from "react-router-dom";
-import TEXT5 from "../assets/images/text5.png"
-// import TEXT6 from "../assets/images/text6.png"
+import TEXT5 from "../assets/images/text5.png";
 
 export default function Fifth() {
   const navigate = useNavigate();
@@ -11,35 +10,32 @@ export default function Fifth() {
   const onCompleteScratch = () => {
     setTimeout(() => {
       navigate("/video");
-    }, 1000);
+    }, 1500);
   };
-    return(
-      <>
-        <div className="screen screen-5">
-                <div className="box">
-                <div className="screen-5-text-1" id="js--sc--container">
-                    <img src={TEXT5} alt="" className="animated zoomIn d-6"/>
-                    <div className="w-[100%] h-[100vh]">
-      <div className="round">
-        <MyScratchCard onComplete={onCompleteScratch} />
-        <h2 className="text-center round-text"
-        style={{
-          textAlign: 'center',
-          paddingTop: '25px',
-          fontSize: '18px'
-        }}
-        >
-          Please slide index fingers on the above circle Life Acidity Free Life
-          Acidity Free Life Acidity{" "}
-        </h2>
+
+  return (
+    <div className="screen screen-5">
+      <div className="box">
+        <div className="screen-5-text-1" id="js--sc--container">
+          <img src={TEXT5} alt="" className="animated zoomIn d-6" />
+          <div className="w-[100%] h-[100vh]">
+            <div className="round">
+              <MyScratchCard onComplete={onCompleteScratch} />
+              <h2
+                className="text-center round-text"
+                style={{
+                  textAlign: "center",
+                  paddingTop: "25px",
+                  fontSize: "18px",
+                }}
+              >
+                Please slide index fingers on the above circle Life Acidity
+                Free Life Acidity Free Life Acidity{" "}
+              </h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-                </div>
-                
-                </div>
-        </div>
-       
-      </>
-      
-    )
- }
+  );
+}
--- src/components/Scratch.jsx
+++ src/components/Scratch.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import ScratchCard from "react-scratchcard-v2";
-import IMGMask from "../assets/images/TrioCircle.png";
-import IMG from "../assets/images/unmask.png";
+import IMGMask from "../assets/images/TrioCircle.png"; 
+import IMG from "../assets/images/unmask.png"; 
 
 const MyScratchCard = ({ onComplete }) => {
   return (
-    <div>
+    <div style={{ position: "relative", width: "257px", height: "257px" }}>
       <ScratchCard
         width={257}
         height={257}
-        image={IMG}  
+        image={IMGMask} 
         finishPercent={90}
         onComplete={onComplete}
       >
-        <img src={IMGMask} style={{ display: 'block', width: '100%', height: '100%' }} alt="Revealed content" />
+        <img
+          src={IMG}
+          style={{
+            display: "block",
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+          alt="Revealed content"
+        />
       </ScratchCard>
     </div>
   );
